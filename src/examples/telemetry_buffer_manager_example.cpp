3835076a18c7e62aef22728b5e962cdd86a77200
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-02-01 21:58:43 +0100

    Check if the clock has been correctly set in the telemetry_buffer_manager_example.cpp

--- src/examples/telemetry_buffer_manager_example.cpp
+++ src/examples/telemetry_buffer_manager_example.cpp
@@ -30,12 +30,16 @@ int main()
 
     yarp::telemetry::BufferManager<int32_t> bm(n_samples);
     bm.setFileName("buffer_manager_test.mat");
-    bm.setNowFunction(now);
+    auto ok = bm.setNowFunction(now);
+    if (!ok) {
+        std::cout << "Problem setting the clock...."<<std::endl;
+        return 1;
+    }
 
     yarp::telemetry::ChannelInfo var_one{ "one", {1,1} };
     yarp::telemetry::ChannelInfo var_two{ "two", {1,1} };
 
-    auto ok = bm.addChannel(var_one);
+    ok = bm.addChannel(var_one);
     ok = ok && bm.addChannel(var_two);
     if (!ok) {
         std::cout << "Problem adding variables...."<<std::endl;
@@ -55,7 +59,13 @@ int main()
 
     yarp::telemetry::BufferManager<int32_t> bm_m(n_samples, true);
     bm_m.setFileName("buffer_manager_test_matrix.mat");
-    bm_m.setNowFunction(now);
+    ok = bm_m.setNowFunction(now);
+    if (!ok) {
+        std::cout << "Problem setting the clock...."<<std::endl;
+        return 1;
+    }
+
+
     std::vector<yarp::telemetry::ChannelInfo> vars{ { "one",{2,3} },
                                    { "two",{3,2} } };
 
@@ -74,7 +84,12 @@ int main()
     yarp::telemetry::BufferManager<double> bm_v("buffer_manager_test_vector.mat",
                                                { {"one",{4,1}},
                                                  {"two",{4,1}} }, n_samples, true);
-    bm_v.setNowFunction(now);
+    ok = bm_v.setNowFunction(now);
+    if (!ok) {
+        std::cout << "Problem setting the clock...."<<std::endl;
+        return 1;
+    }
+
 
     for (int i = 0; i < 10; i++) {
         bm_v.push_back({ i+1.0, i+2.0, i+3.0, i+4.0  }, "one");
