6d7915287742a11e1d8b55896d8de787f20e806c
Nicogene <nicolo.genesio@iit.it>
2023-09-06 14:54:21 +0200

    Port unit tests to catch v3
    It fixes #183

--- test/BufferManagerTest.cpp
+++ test/BufferManagerTest.cpp
@@ -9,7 +9,7 @@
 #define CATCH_CONFIG_MAIN
 
 #include <robometry/BufferManager.h>
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <vector>
 #include <mutex>
 #include <thread>
